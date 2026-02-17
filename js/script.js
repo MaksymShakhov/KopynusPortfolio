347e6d6b3925ac8ac61ed309c3e05e0ede2e800c
Maksym Shakhov <kos782226@gmail.com>
2026-02-17 19:20:16 +0100

    Commit1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
+
 function opentab(tabname) {
     for (tablink of tablinks) {
         tablink.classList.remove("active-link");
@@ -14,15 +15,12 @@ function opentab(tabname) {
 function openPortfolioTab(tabname) {
     var portfolioTabLinks = document.querySelectorAll('.portfolio-tab-link');
     var portfolioTabContents = document.querySelectorAll('.portfolio-tab-content');
-    
     for (var i = 0; i < portfolioTabLinks.length; i++) {
         portfolioTabLinks[i].classList.remove('active-portfolio-tab');
     }
-    
     for (var i = 0; i < portfolioTabContents.length; i++) {
         portfolioTabContents[i].classList.remove('active-portfolio-tab');
     }
-    
     event.currentTarget.classList.add('active-portfolio-tab');
     document.getElementById(tabname).classList.add('active-portfolio-tab');
 }
@@ -47,10 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('click', function(event) {
     var mobileMenu = document.getElementById('mobileMenu');
     var menuToggle = document.querySelector('.menu-toggle');
-    
-    if (mobileMenu.classList.contains('active') && 
-        !mobileMenu.contains(event.target) && 
-        !menuToggle.contains(event.target)) {
+    if (mobileMenu.classList.contains('active') && !mobileMenu.contains(event.target) && !menuToggle.contains(event.target)) {
         closeMobileMenu();
     }
 });
